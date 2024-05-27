fe0aa3dd8c43bdf84a37842db3a8f2f2512a8d12
BehinaBhr <be.bhr77@gmail.com>
2024-05-27 12:54:11 -0700

    Initialize React project and add reusable Post component for displaying dynamic values

---
+++ react-starting-project/src/App.jsx
@@ -0,0 +1,13 @@
+import Post from "./components/Post";
+function App() {
+  return (
+    <main>
+      <h1>Hello World!</h1>
+      <Post></Post> 
+      <Post />
+      <Post />
+    </main>
+  );
+}
+
+export default App;
