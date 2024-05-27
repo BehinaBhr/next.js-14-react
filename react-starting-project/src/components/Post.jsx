2be522da267f4ea411c5ce5d1565d8563691e511
BehinaBhr <be.bhr77@gmail.com>
2024-05-27 13:47:20 -0700

    Add CSS module styling to post component

--- react-starting-project/src/components/Post.jsx
+++ react-starting-project/src/components/Post.jsx
@@ -1,10 +1,15 @@
+import styles from  "./Post.module.css";
 function Post(props) {
-
   return (
-    <div>
-      <p>{props.author}</p>
-      <p>{props.body}</p>
+    // inline styles:
+    // dash "-" is not allowed in plain property / variable names in JavaScript: "textAlign" instead of "text-align"
+    // it is not special double curly braces syntax, instead it's still the same single {} syntax for dunamic values with an object passed as a value to the style prop.
+    // <div style={{ color: "red", textAlign: "left" }}>
 
+    // scope CSS styles to not clash with others globally in index.css
+    <div className={styles.post}>
+      <p className={styles.author}>{props.author}</p>
+      <p className={styles.text}>{props.body}</p>
     </div>
   );
 }
