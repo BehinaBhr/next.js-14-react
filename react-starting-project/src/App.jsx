755ae641ce0a90fc23adf2f4d7ced6f5297af63a
BehinaBhr <be.bhr77@gmail.com>
2024-05-27 13:04:16 -0700

    Refactor Post component to pass data using props for reusability

--- react-starting-project/src/App.jsx
+++ react-starting-project/src/App.jsx
@@ -3,9 +3,8 @@ function App() {
   return (
     <main>
       <h1>Hello World!</h1>
-      <Post></Post> 
-      <Post />
-      <Post />
+      <Post author="Maximilian" body="React.js is awesome!" />
+      <Post author="Manual" body="Check out the full course!" />
     </main>
   );
 }
--- react-starting-project/src/components/Post.jsx
+++ react-starting-project/src/components/Post.jsx
@@ -1,15 +1,9 @@
-const name = ["Maximilian", "Manual"];
-
-function Post() {
-// Math.random() generates pseudo-random floating-point number = decimal between 0 (inclusive) and 1 (exclusive).
-// If the random number is greater than 0.5 (which happens with a 50% chance),the expression evaluates to true.
-// condition ? expressionIfTrue : expressionIfFalse 
-  const chosenName = Math.random() > 0.5 ? name[0] : name[1];
+function Post(props) {
 
   return (
     <div>
-      <p>name: {chosenName}</p>
-      <p>React.js is awesome!</p>
+      <p>{props.author}</p>
+      <p>{props.body}</p>
 
     </div>
   );
