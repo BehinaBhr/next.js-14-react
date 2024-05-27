9a8bb733e067dc80619025d001adde80b1805334
BehinaBhr <be.bhr77@gmail.com>
2024-05-27 16:15:16 -0700

    Lift state up to PostsList component containing NewPost component

--- react-starting-project/src/components/NewPost.jsx
+++ react-starting-project/src/components/NewPost.jsx
@@ -1,27 +1,17 @@
 import classes from "./NewPost.module.css";
-import { useState } from "react";
 
 // label normally has for attribute; but not allowed in JSX code, instead htmlFor.
-// functions in React that start with use = React Hooks.
-// const [currentValue, setCurrentValue] = useState("")
 
-function NewPost() {
-  const [enteredBody, setEnteredBody] = useState("");
-
-  function changBodyHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
+function NewPost(props) {
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changBodyHandler} />
+        <textarea id="body" required rows={3} onChange={props.onBodyChange} />
       </p>
-      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={props.onAuthorChange} />
       </p>
     </form>
   );
--- react-starting-project/src/components/PostsList.jsx
+++ react-starting-project/src/components/PostsList.jsx
@@ -1,12 +1,31 @@
-import styles from  "./PostsList.module.css";
-import Post from "../components/Post";
+import styles from "./PostsList.module.css";
+import Post from "./Post";
+import NewPost from "./NewPost";
+import { useState } from "react";
+
+// functions in React that start with use = React Hooks.
+// const [currentValue, setCurrentValue] = useState("")
+//lifting the state up: If we have state that's manipulated in component A but needed in component B, you should lift the state up to a component that has access to both components that need the state.
 
 function PostsList() {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function changBodyHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function changeAuthorHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
   return (
-    <ul className={styles.posts}>
-      <Post author="Maximilian" body="React.js is awesome!" />
-      <Post author="Manual" body="Check out the full course!" />
-    </ul>
+    <>
+      <NewPost onBodyChange={changBodyHandler} onAuthorChange={changeAuthorHandler} />
+      <ul className={styles.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Manual" body="Check out the full course!" />
+      </ul>
+    </>
   );
 }
 export default PostsList;
